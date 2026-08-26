5064ce67db4a1d6f265c9e1aa8e1754dc19294b6
DarkRod <diablo120788@yahoo.fr>
2026-08-26 19:25:58 +0200

    .

--- api.js
+++ api.js
@@ -1,13 +1,16 @@
 import express from "express";
-import { verseData } from "./main.js";
+import { verseData, startVerseTime } from "./main.js";
+import { getLocationByName } from "./HelperFunctions.js";
 
 const app = express();
 const port = 3000;
 
 console.log(verseData);
 
-app.get("/", (req, res) => {
-  res.json({ message: "VerseTime API is running.", verseData: verseData });
+app.get("/", async (req, res) => {
+  const location = getLocationByName(req.query.location_name);
+  await startVerseTime();
+  res.json({ message: "VerseTime API is running.", verseData: verseData, location: location });
 });
 
 app.listen(port, () => {
--- main.js
+++ main.js
@@ -1,14 +1,37 @@
 import Settings from "./classes/app/Preferences.js";
 import DB from "./classes/app/Database.js";
-// NE PAS importer UI ici
 import { convertHoursToTimeString } from "./HelperFunctions.js";
+
+const isBrowser =
+  typeof window !== "undefined" && typeof document !== "undefined";
+const isServer = !isBrowser;
+
+let UI = null;
+if (isBrowser) {
+  const { default: UIModule } = await import("./classes/app/UserInterface.js");
+  UI = UIModule;
+}
 
 export let verseData = null;
 
-// INIT
-async function startVerseTime() {
+export async function startVerseTime() {
   await DB.createDatabase();
+
+  if (isBrowser && UI) {
+    DB.locations.sort((a, b) => a.NAME.localeCompare(b.NAME));
+    UI.populateLocationList();
+    UI.setupEventListeners();
+  }
+
+  if (isBrowser) {
+    checkHash();
+  }
+
   Settings.load();
+
+  if (isBrowser && UI) {
+    setInterval(update, 250);
+  }
 
   // DEBUG ALL DATA
   const data = {
@@ -37,10 +60,41 @@ async function startVerseTime() {
   };
 
   verseData = data;
+  console.log("Verse Data:", data);
   return data;
 }
 
-// Exécuter l'initialisation
+export function update() {
+  if (DB.locations.length === 0) return;
+  if (UI) UI.update();
+}
+
+function checkHash() {
+  if (!isBrowser) return;
+  const hash = window.location.hash;
+  if (hash === "") return;
+
+  const hashParts = hash.replace("#", "").replaceAll("_", " ").split("@");
+  const locationName = hashParts[0];
+
+  if (hashParts[1] !== undefined && UI) {
+    UI.setCustomTime(hashParts[1], true);
+  }
+
+  if (UI) UI.setMapLocation(locationName);
+}
+
+if (isBrowser) {
+  window.addEventListener(
+    "hashchange",
+    () => {
+      if (window.suppressReload) return;
+      window.location.reload(true);
+    },
+    false,
+  );
+}
+
 await startVerseTime();
 
-export default verseData;
+export default { verseData, startVerseTime};
